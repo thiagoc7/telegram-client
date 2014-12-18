7e07877f25befc974ce5b3a0a158ac573c88c087
Thiago <thiagobocorrea@gmail.com>
2014-12-18 16:26:26 -0200

    nav-bar component

---
+++ app/components/nav-bar.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  userProfile: function() {
+    return this.get('session.user');
+  }.property('session'),
+
+  userAuthenticated: function() {
+    return this.get('session.isAuthenticated');
+  }.property('session')
+
+});
--- app/controllers/dash.js
+++ app/controllers/dash.js
@@ -1,12 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
-  userProfile: function() {
-    return this.get('session.user');
-  }.property('session'),
-
-  userAuthenticated: function() {
-    return this.get('session.isAuthenticated');
+  userSession: function() {
+    return this.get('session');
   }.property('session')
 
 });
