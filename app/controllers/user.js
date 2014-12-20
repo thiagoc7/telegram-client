ac31a70987d629a85ebd003d0af81d6ac726847f
Thiago <thiagobocorrea@gmail.com>
2014-12-20 09:54:16 -0200

    profile basic css

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -3,6 +3,16 @@ import Ember from 'ember';
 export default Ember.ObjectController.extend({
   userSession: function() {
     return this.get('session');
-  }.property('session')
+  }.property('session'),
+
+  actions: {
+    follow: function (user) {
+      return user; //follow stuff
+    },
+
+    unfollow: function (user) {
+      return user; //follow stuff
+    }
+  }
 
 });
