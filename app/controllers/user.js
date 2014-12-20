4ccdf7b5975338f506b2f2118a31a2b808d9f8d7
Thiago <thiagobocorrea@gmail.com>
2014-12-20 10:25:09 -0200

    profile navigation and follow-user component

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -3,16 +3,7 @@ import Ember from 'ember';
 export default Ember.ObjectController.extend({
   userSession: function() {
     return this.get('session');
-  }.property('session'),
+  }.property('session')
 
-  actions: {
-    follow: function (user) {
-      return user; //follow stuff
-    },
-
-    unfollow: function (user) {
-      return user; //follow stuff
-    }
-  }
 
 });
--- app/routes/user.js
+++ app/routes/user.js
@@ -3,5 +3,15 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function(params) {
     return this.store.find('user', params.user_id);
+  },
+
+  actions: {
+    follow: function (user) {
+      return user; //follow stuff
+    },
+
+    unfollow: function (user) {
+      return user; //follow stuff
+    }
   }
 });
