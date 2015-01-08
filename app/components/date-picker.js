c62ce42fe7e10ac3c3a13830fe5f986e9111caf8
Thiago <thiagobocorrea@gmail.com>
2015-01-08 16:09:27 -0200

    datepicker component, working need to fix key input

---
+++ app/components/date-picker.js
@@ -0,0 +1,44 @@
+import Ember from 'ember';
+//import { moment } from 'ember-moment/computed';
+
+export default Ember.Component.extend({
+  tagName: 'input',
+  type: 'text',
+  attributeBindings: ['value'],
+
+  value: new Date(),
+  momentFormat: 'DD/MM/YYYY',
+  pickerFormat: 'dd/mm/yyyy',
+  displayDate: function() {
+    var date = this.get('value'),
+      format = this.get('momentFormat');
+    return moment(date).format(format);
+  }.property('value', 'format'),
+
+  didInsertElement: function () {
+    this.$().val(this.get('displayDate'));
+    var picker = this.$().datepicker(
+      {
+        format: this.get('pickerFormat'),
+        autoclose: true
+      }
+    );
+
+    var component = this;
+    this.set('_picker', picker);
+    picker.on('changeDate', function(event) {
+      Ember.run(function () {
+        component.newDate(event);
+      });
+    });
+  },
+
+  willDestroyElement: function () {
+    this.$().datepicker('remove');
+  },
+
+  // TODO check why can' user enter key
+  newDate: function (e) {
+    this.set('value', e.date);
+  }
+});
