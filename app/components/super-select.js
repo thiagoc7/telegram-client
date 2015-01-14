2e4a9e02e85db51ba88d1bd1e9803bc66b3c255a
Thiago <thiagobocorrea@gmail.com>
2015-01-14 09:57:22 -0200

    super select navigation working

--- app/components/super-select.js
+++ app/components/super-select.js
@@ -9,6 +9,7 @@ export default Ember.Component.extend({
   activeItem: null,
 
   placeholder: 'select options',
+
   label: function () {
     if (this.get('selectedItem')) {
       return this.get('selectedItem.object.name');
@@ -38,17 +39,26 @@ export default Ember.Component.extend({
     var result = this.get('content').filter(function(item) {
       if (item.name === text) { return true; }
     });
-    return result.length === 0 && text !== '';
-  }.property('content', 'inputText'),
+    //return result.length === 0 && text !== '';
+    if (result.length === 0 && text !== '') {
+      var object = Ember.Object.create({
+        name: '+ new: ' + text,
+        text: text,
+        newObject: true
+      });
+      this.get('resultList').pushObject(
+        Ember.Object.create({
+          object: object,
+          active: false
+        })
+      );
+    }
+  }.observes('content', 'inputText'),
 
   actions: {
     selectItem: function (item) {
       this.setActiveItem(item);
       this.chooseActive();
-    },
-
-    createItem: function () {
-      this.set('inputIsFocused', false);
     },
 
     openSelect: function () {
@@ -57,52 +67,69 @@ export default Ember.Component.extend({
     }
   },
 
-  selectedIndex: function() {
-    var selectedItemIndex = this.get('resultList').indexOf(this.get('selectedItem'));
+  activeIndex: function() {
+    var activeItemIndex = this.get('resultList').indexOf(this.get('activeItem'));
     var length = this.get('resultList').get('length');
-    if (selectedItemIndex && length > 0 && selectedItemIndex <= length) {
-      return selectedItemIndex;
+    if (activeItemIndex !== -1 && length > 0 && activeItemIndex <= length) {
+      return activeItemIndex;
     } else {
       return -1;
     }
-  }.property('selectedItem', 'resultList.[]'),
+  }.property('activeItem', 'resultList.[]'),
 
   setActiveItem: function (item) {
-    var originalList = this.get('formatedList');
-    var cleanList = originalList.map(function(object) {
-      return object.set('active', false);
+    this.get('formatedList').forEach(function(object) {
+      object.set('active', false);
     });
-    this.set('formatedList', cleanList);
     item.set('active', true);
     this.set('activeItem', item);
   },
 
   chooseActive: function () {
+    if (this.get('activeItem').object.newObject) {
+      alert ('new');
+    }
     this.set('selectedItem', this.get('activeItem'));
     this.set('inputIsFocused', false);
   },
 
   navigateOnKeyDown: function(event) {
+    function decreaseInQueue(val, min, max) {
+      val -= 1;
+      if (val < min) {
+        val = max;
+      }
+      return val;
+    }
+
+    function increaseInQueue(val, min, max) {
+      val += 1;
+      if (val > max) {
+        val = min;
+      }
+      return val;
+    }
+
     switch(event.keyCode) {
       case 27: //esc
         this.set('inputIsFocused', false);
-        break;
+        return false;
 
       case 38: //up-arrow
-        var indexUp = this.get('selectedIndex') - 1;
+        var indexUp = decreaseInQueue(this.get('activeIndex'), 0, this.get('resultList.length') - 1);
         var itemUp = this.get('resultList').objectAt(indexUp);
         this.setActiveItem(itemUp);
-        break;
+        return false;
 
       case 40: //down-arrow
-        var indexDown = this.get('selectedIndex') + 1;
+        var indexDown = increaseInQueue(this.get('activeIndex'), 0, this.get('resultList.length') - 1);
         var itemDown = this.get('resultList').objectAt(indexDown);
         this.setActiveItem(itemDown);
-        break;
+        return false;
 
       case 13: //enter
         this.chooseActive();
-        break;
+        return false;
     }
   }.on('keyDown')
 });
