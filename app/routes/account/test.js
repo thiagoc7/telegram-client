c304ad52c101b0e9d21340dea2bba0b37f18dc35
Thiago <thiagobocorrea@gmail.com>
2015-01-17 18:30:24 -0200

    caching

--- app/routes/account/test.js
+++ app/routes/account/test.js
@@ -10,21 +10,51 @@ export default Ember.Route.extend({
     }
   },
 
+  postsCache: null,
+  postsPage: null,
+  posts2Cache: null,
+  posts2Page: null,
+
   model: function(params) {
-    var store = this.store;
+    var postsPromise;
+    var posts2Promise;
+
+    if (this.get('postsPage') === params.page && this.get('postsCache') !== null) {
+      var postsDeferred = Ember.RSVP.defer();
+      postsDeferred.resolve(this.get('postsCache'));
+      postsPromise = postsDeferred.promise;
+    } else {
+      this.set('postsPage', params.page);
+      postsPromise = this.store.find('post', {
+        userId: 'th',
+        page: params.page,
+        size: params.size
+      });
+    }
+
+    if (this.get('posts2Page') === params.page2 && this.get('posts2Cache') !== null) {
+      var posts2Deferred = Ember.RSVP.defer();
+      posts2Deferred.resolve(this.get('postsCache'));
+      posts2Promise = posts2Deferred.promise;
+    } else {
+      this.set('posts2Page', params.page2);
+      posts2Promise = this.store.find('post', {
+        userId: 'th',
+        page: params.page2,
+        size: params.size
+      });
+    }
+
+    var route = this;
     return Ember.RSVP.hash({
-      posts: store.find('post',
-        {
-          userId: 'th',
-          page: params.page,
-          size: params.size
-        }),
-      posts2: store.find('post',
-        {
-          userId: 'th',
-          page: params.page2,
-          size: params.size2
-        })
+      posts: postsPromise,
+      posts2: posts2Promise
+    }).then(function(results) {
+      route.setProperties({
+        postsCache: results.posts,
+        posts2Cache: results.posts
+      });
+      return results;
     });
   }
 });
